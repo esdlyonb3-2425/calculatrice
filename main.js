48dcada85ef1878d0027242cfc1672061db218c6
esdb1 <pierre.lacognata@mail-esd.com>
2022-10-19 11:35:19 +0200

    base math

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const chiffres = document.querySelectorAll('.bouton')
 const ecran = document.querySelector('.ecran')
+
+
 const boutonEgal = document.querySelector('.egal')
 
 let resultat = 0
@@ -7,12 +9,24 @@ ecran.innerHTML = resultat
 
 function calcule(){
 
+
+   resultat = eval(ecran.innerHTML)
+    ecran.innerHTML = resultat
+
+
 }
 
 chiffres.forEach((chiffre)=>{
     chiffre.addEventListener('click',()=>{
-        console.log(chiffre.id)
-        ecran.innerHTML += chiffre.id
+
+        if(ecran.innerHTML == '0'){
+            ecran.innerHTML = chiffre.id
+
+        }else{
+            ecran.innerHTML += chiffre.id
+
+        }
+
 
     })
 })
